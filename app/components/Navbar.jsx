e368100d8e46f9cb3b46f4ad270ed9c2fbec385d
Anisha Hirani <anisha.hirani11@gmail.com>
2025-09-13 19:13:21 +0100

    centering and padding navbar with button css on contact

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-      <nav>
-        <a href="">
+      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+        <a href="#top">
 
         </a>
 
-        <ul>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py3'>
           <li><a href="#top">Home</a></li>
           <li><a href="#about">About me</a></li>
           <li><a href="#services">Services</a></li>
@@ -17,7 +17,7 @@ const Navbar = () => {
         </ul>
 
         <div>
-          <a href="#contact">Contact</a>
+          <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact</a>
         </div>
       </nav>
     </>

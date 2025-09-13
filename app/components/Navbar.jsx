0c5c18a0f171d1062e52202fa54fc8fa975c0bdb
Anisha Hirani <anisha.hirani11@gmail.com>
2025-09-13 19:36:36 +0100

    adding dark/light mode toggle button with css

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
         </a>
 
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py3'>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-amber-100 shadow-sm bg-opacity-50'>
           <li><a href="#top">Home</a></li>
           <li><a href="#about">About me</a></li>
           <li><a href="#services">Services</a></li>
@@ -16,7 +16,9 @@ const Navbar = () => {
           <li><a href="#contact">Contact</a></li>
         </ul>
 
-        <div>
+        <div className='flex items-center gap-4'>
+          <button>Dark</button>
+
           <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact</a>
         </div>
       </nav>

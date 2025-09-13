d1d9a583767bd3ad288cf5159084ba1345a30af0
Anisha Hirani <anisha.hirani11@gmail.com>
2025-09-13 19:05:47 +0100

    created navbar with basic tabs

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <a href="">
+
+        </a>
+
+        <ul>
+          <li><a href="#top">Home</a></li>
+          <li><a href="#about">About me</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#work">My Work</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div>
+          <a href="#contact">Contact</a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar

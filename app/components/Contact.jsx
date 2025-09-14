7421e30bee795e705d4b8767d6661680bc24f46c
Anisha Hirani <anisha.hirani11@gmail.com>
2025-09-14 21:22:28 +0100

    import code from web3forms

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+  
   return (
     <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4 className='text-center mb-2 text-lg'>Let's Connect</h4>

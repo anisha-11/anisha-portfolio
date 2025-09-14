13efa6cbf1c5fde7e7f23fb8ceb864a44ee38454
Anisha Hirani <anisha.hirani11@gmail.com>
2025-09-14 20:37:00 +0100

    added service cards in grids with css

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,9 +1,24 @@
+import { assets, serviceCards } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Services = () => {
   return (
-    <div>
-        
+    <div id='services' className='w-full px-[12%] py-10 scroll-mt-20'>
+      <h4 className='text-center mb-2 text-lg'>What I Offer</h4>
+      <h2 className='text-center text-5xl'>My Services</h2> 
+      <p className='text-center max-w-2xl mx-auto mt-5 mb-12'>Lorem Ipsum</p>
+
+      <div className='grid grid-cols-4 gap-6 my-10'>
+        {serviceCards.map(({icon, title, description, link}, index)=>(
+          <div key={index} className='border border-gray-400 rounded-lg px-8 py-12 cursor-pointer'>
+            <Image src={icon} alt='' className='w-10'/>
+            <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
+            <p className='text-sm text-gray-600 leading-5'>{description}</p>
+            <a href={link} className='flex items-center gap-2 text-sm mt-5'>Read More {/*<Image src={assets.arrow} className='w-4'/>*/}</a>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }

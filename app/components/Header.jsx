27ad04655f71c67e9f0fbea8a74fd1ded9541fa6
Anisha Hirani <anisha.hirani11@gmail.com>
2025-09-14 22:45:47 +0100

    updating header text

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,8 +9,9 @@ const Header = () => {
       <Image src={assets.profile_picture} alt="" className=' rounded-full w-32' />
       </div>
       <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 text-amber-300'>Hi! I'm Anisha Hirani</h3>
-      <h1 className='text-3xl sm:text-6xl lg:text-[66px] text-amber-300'>Software Engineer based in London</h1>
-      <p className='max-w-2xl mx-auto text-amber-300'>I am currently a Salesforce Developer from London with almost 2 years of experience at Yoox Net-a-Porter Group.</p>
+      <h1 className='text-3xl sm:text-6xl lg:text-[66px] text-amber-300'>Software Engineer</h1>
+      <p className='max-w-2xl mx-auto text-amber-300'>I am based in London and currently working as a Salesforce Developer 
+      with almost 2 years of experience working for Revolent Group whilst on placement at Yoox Net-a-Porter Group.</p>
       <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
         <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>Contact
         {/* <Image src={assets.arrow} alt="" className='w-4'/> */}</a>

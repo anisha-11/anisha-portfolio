23ee025d519f5a8d4fa985683a24b6d64b77d49b
Anisha Hirani <anisha.hirani11@gmail.com>
2025-09-14 19:48:06 +0100

    connect about scroll to about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='w-full px-[12%] py-10 scroll-mt-20'>
+    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4 className='text-center mb-2 text-lg'>Introduction</h4>
       <h2 className='text-center text-5xl'>About me</h2>
 

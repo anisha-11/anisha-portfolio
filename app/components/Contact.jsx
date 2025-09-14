eda212d08c3ce68af53ee2d69b4411e066b2c786
Anisha Hirani <anisha.hirani11@gmail.com>
2025-09-14 21:19:20 +0100

    creating form structure with css - no functionality yet

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      
+    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20'>
+      <h4 className='text-center mb-2 text-lg'>Let's Connect</h4>
+      <h2 className='text-center text-5xl'>Get In Touch</h2> 
+      <p className='text-center max-w-2xl mx-auto mt-5 mb-12'>
+      I'd love to hear from you! If you have any questions, comments or feedback, please fill the form below</p>
+
+      <form className='max-w-2xl mx-auto'>
+        <div className='grid grid-cols-2 gap-6 mt-10 mb-8'>
+          <input type="text" placeholder='Enter your name' required 
+          className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-amber-300'/>
+          <input type="email" placeholder='Enter your email' required 
+          className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-amber-300'/>
+        </div>
+        <textarea rows="6" placeholder='Enter your message' required
+        className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-amber-300 mb-6'>
+        </textarea>
+        <button type='submit' 
+        className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto'>
+        Submit
+        </button>
+        <p className='mt-4'>Sending...</p>
+      </form>
     </div>
   )
 }

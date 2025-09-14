edb74eb20d3fc534e147095a42ab54b53eed8ee2
Anisha Hirani <anisha.hirani11@gmail.com>
2025-09-14 20:02:06 +0100

    add image and grids to about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { assets, infoList } from '@/assets/assets';
+import React from 'react';
+import Image from 'next/image';
 
 const About = () => {
   return (
@@ -7,13 +9,20 @@ const About = () => {
       <h2 className='text-center text-5xl'>About me</h2>
 
       <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
-        <div className='w-64 sm:w-80 rounded-3xl max-w-none'>Image</div>
-        <div className='flex-1'>Image2
-        <p>Adding a para to say i am experienced salesforce dev with blah blah and blah</p>
+        <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
+        <Image src={assets.profile_picture} alt="" className='w-full rounded-3xl'/></div>
+        <div className='flex-1'>
+        <p className='mb-10 max-w-2xl'>Adding a para to say i am experienced salesforce dev with blah blah and blah</p>
 
-        {/* <ul>
-          asset.js 56
-        </ul> */}
+        <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
+          {infoList.map(({icon, iconDark, title, description}, index)=>(
+            <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer' key={index}>
+              <Image src={icon} alt={title} className='w-7 mt-3'/>
+              <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
+              <p className='text-gray-600 text-sm'>{description}</p>
+            </li>
+          ))}
+        </ul>
 
         <h4>Tools I use</h4>
 

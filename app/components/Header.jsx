2bdf1e8dfd2ffc2806dfc9a0a74ce9f78d946a27
Anisha Hirani <anisha.hirani11@gmail.com>
2025-09-14 19:16:15 +0100

    added profile pic image

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { assets } from '@/assets/assets';
+import React from 'react';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
-      <div>Profile Picture</div>
+      <div>
+      <Image src={assets.profile_picture} alt="" className='w-28 cursor-pointer mr-14 rounded-full justify-center' />
+      </div>
       <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 text-amber-300'>Hi! I'm Anisha Hirani</h3>
       <h1 className='text-3xl sm:text-6xl lg:text-[66px] text-amber-300'>Software Engineer based in London</h1>
       <p className='max-w-2xl mx-auto text-amber-300'>I am currently a Salesforce Developer with almost 2 years of experience at YNAP</p>

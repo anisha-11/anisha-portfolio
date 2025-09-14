0c2edb35b43d8915de99da3c77ac666264859723
Anisha Hirani <anisha.hirani11@gmail.com>
2025-09-14 21:32:13 +0100

    adding small css and submitting without api key exposed

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
-  const [result, setResult] = React.useState("");
+  const [result, setResult] = useState("");
 
   const onSubmit = async (event) => {
     event.preventDefault();
     setResult("Sending....");
     const formData = new FormData(event.target);
 
-    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+    formData.append("access_key", "ACCESS_KEY");
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
@@ -25,7 +25,7 @@ const Contact = () => {
       setResult(data.message);
     }
   };
-  
+
   return (
     <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4 className='text-center mb-2 text-lg'>Let's Connect</h4>
@@ -33,21 +33,21 @@ const Contact = () => {
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12'>
       I'd love to hear from you! If you have any questions, comments or feedback, please fill the form below</p>
 
-      <form className='max-w-2xl mx-auto'>
+      <form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
         <div className='grid grid-cols-2 gap-6 mt-10 mb-8'>
           <input type="text" placeholder='Enter your name' required 
-          className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-amber-300'/>
+          className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-amber-300' name='name'/>
           <input type="email" placeholder='Enter your email' required 
-          className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-amber-300'/>
+          className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-amber-300' name='email'/>
         </div>
         <textarea rows="6" placeholder='Enter your message' required
-        className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-amber-300 mb-6'>
+        className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-amber-300 mb-6' name='message'>
         </textarea>
         <button type='submit' 
         className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto'>
         Submit
         </button>
-        <p className='mt-4'>Sending...</p>
+        <p className='mt-4'>{result}</p>
       </form>
     </div>
   )
